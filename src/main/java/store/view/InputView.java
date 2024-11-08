da590fab2746b4b47775d1b4708d4bea8aa83fed
eb9862 <luclead622@gmail.com>
2024-11-09 00:57:05 +0900

    refactor: InputView, OutputView 클래스의 공통 메서드 분리
    추상 클래스 View 로 분리

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -4,11 +4,11 @@ import static store.view.Constant.ORDER_MESSAGE;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView{
+public class InputView extends View {
 
     public static String readOrder() {
-        OutputView.printNewLine();
-        OutputView.printMessage(ORDER_MESSAGE);
+        printNewLine();
+        printMessage(ORDER_MESSAGE);
         return Console.readLine();
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -9,7 +9,7 @@ import java.util.List;
 import store.domain.Inventory;
 import store.domain.Product;
 
-public class OutputView {
+public class OutputView extends View {
 
     public static void printInventory(Inventory inventory) {
         printMessage(INTRODUCE_PRODUCT_MESSAGE);
@@ -28,15 +28,7 @@ public class OutputView {
         printMessage(PRODUCT_INFO_ZERO_QUANTITY.format(product.getName(), product.getPrice(), product.getPromotionName()));
     }
 
-    public static void printMessage(String message) {
-        System.out.println(message);
-    }
-
     public static void printError(String errorMessage) {
         printMessage(ERROR_TAG + errorMessage);
-    }
-
-    public static void printNewLine() {
-        System.out.println();
     }
 }
