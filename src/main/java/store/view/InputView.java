fa93ea82c7ad010aa454d503400fa5092f4a41b3
eb9862 <luclead622@gmail.com>
2024-11-10 15:56:06 +0900

    feat: 혜택과 재고부족에 대한 메시지 출력 및 대답 입력 받기

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -1,6 +1,8 @@
 package store.view;
 
 import static store.view.Constant.ORDER_MESSAGE;
+import static store.view.MessageTemplate.PROMOTION_BENEFIT;
+import static store.view.MessageTemplate.PROMOTION_QUANTITY_SHORTAGE;
 
 import camp.nextstep.edu.missionutils.Console;
 
@@ -9,6 +11,18 @@ public class InputView extends View {
     public static String readOrder() {
         printNewLine();
         printMessage(ORDER_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static String readForAdditionalItem(String productName) {
+        printNewLine();
+        printMessage(PROMOTION_BENEFIT.format(productName));
+        return Console.readLine();
+    }
+
+    public static String readForOutOfStock(String productName, int shortageQuantity) {
+        printNewLine();
+        printMessage(PROMOTION_QUANTITY_SHORTAGE.format(productName, shortageQuantity));
         return Console.readLine();
     }
 }
