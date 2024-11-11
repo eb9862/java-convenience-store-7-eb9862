a33cf35f3de50e186177ece9235011af18a2ca6d
eb9862 <luclead622@gmail.com>
2024-11-11 23:25:27 +0900

    refactor: 파일 내부에 정보가 담긴 index를 enum 클래스로 구현

--- src/main/java/store/util/Parser.java
+++ src/main/java/store/util/Parser.java
@@ -1,5 +1,12 @@
 package store.util;
 
+import static store.util.FileIndex.PRODUCTS_PRICE;
+import static store.util.FileIndex.PRODUCTS_QUANTITY;
+import static store.util.FileIndex.PROMOTIONS_BUY;
+import static store.util.FileIndex.PROMOTIONS_END_DATE;
+import static store.util.FileIndex.PROMOTIONS_GET;
+import static store.util.FileIndex.PROMOTIONS_START_DATE;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -11,23 +18,23 @@ public class Parser {
 
     public static List<Object> parseProductInfo(String line) {
         List<Object> productInfo = new ArrayList<>(List.of(line.split(",")));
-        String productPrice = (String) productInfo.get(1);
-        productInfo.set(1, Integer.parseInt(productPrice));
-        String productQuantity = (String) productInfo.get(2);
-        productInfo.set(2, Integer.parseInt(productQuantity));
+        String productPrice = (String) productInfo.get(PRODUCTS_PRICE.getIndex());
+        productInfo.set(PRODUCTS_PRICE.getIndex(), Integer.parseInt(productPrice));
+        String productQuantity = (String) productInfo.get(PRODUCTS_QUANTITY.getIndex());
+        productInfo.set(PRODUCTS_QUANTITY.getIndex(), Integer.parseInt(productQuantity));
         return productInfo;
     }
 
     public static List<Object> parsePromotionInfo(String line) {
         List<Object> promotionInfo = new ArrayList<>(List.of(line.split(",")));
-        String buy = (String) promotionInfo.get(1);
-        promotionInfo.set(1, Integer.parseInt(buy));
-        String get = (String) promotionInfo.get(2);
-        promotionInfo.set(2, Integer.parseInt(get));
-        String startDate = (String) promotionInfo.get(3);
-        promotionInfo.set(3, toLocalDateTime(startDate));
-        String endDate = (String) promotionInfo.get(4);
-        promotionInfo.set(4, toLocalDateTime(endDate));
+        String buy = (String) promotionInfo.get(PROMOTIONS_BUY.getIndex());
+        promotionInfo.set(PROMOTIONS_BUY.getIndex(), Integer.parseInt(buy));
+        String get = (String) promotionInfo.get(PROMOTIONS_GET.getIndex());
+        promotionInfo.set(PROMOTIONS_GET.getIndex(), Integer.parseInt(get));
+        String startDate = (String) promotionInfo.get(PROMOTIONS_START_DATE.getIndex());
+        promotionInfo.set(PROMOTIONS_START_DATE.getIndex(), toLocalDateTime(startDate));
+        String endDate = (String) promotionInfo.get(PROMOTIONS_END_DATE.getIndex());
+        promotionInfo.set(PROMOTIONS_END_DATE.getIndex(), toLocalDateTime(endDate));
         return promotionInfo;
     }
 
