e659d39a1433648c4d8861300a85ca364932c63f
eb9862 <luclead622@gmail.com>
2024-11-11 23:06:43 +0900

    refactor: 인스턴스 생성을 막기 위해 private 기본 생성자 설정

--- src/main/java/store/util/InformationMessage.java
+++ src/main/java/store/util/InformationMessage.java
@@ -2,6 +2,8 @@ package store.util;
 
 public class InformationMessage {
 
+    private InformationMessage() {}
+
     public static final String WELCOME_MESSAGE = "안녕하세요. W편의점입니다.";
     public static final String INTRODUCE_PRODUCT_MESSAGE = "현재 보유하고 있는 상품입니다.";
     public static final String ORDER_MESSAGE = "구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])";
